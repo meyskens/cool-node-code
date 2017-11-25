34d4db6c105a4f37627ba4cc48db3748fc080db8
Maartje Eyskens <meyskens@me.com>
2017-11-25 14:30:12 +0100

    add stringtest

---
+++ test/stringtest/stringtest.ino
@@ -0,0 +1,42 @@
+#line 2 "test2.ino"
+#include <ArduinoUnit.h>
+
+bool compare(char data[], char compare[], int len) {
+    for (int i = 0; i < len; i++){
+        if (data[i] != compare[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+test(compareSucceed) {
+    char test1[] = "KEY1234"
+    char test2[] = "KEY"
+    bool ok = compare(test1, test2, 3)
+    assertTrue(ok)
+}
+
+test(compareFalse) {
+    char test1[] = "KOY1234"
+    char test2[] = "KEY"
+    bool ok = compare(test1, test2, 3)
+    assertFalse(ok)
+}
+
+test(concatStrint) {
+    char test1[] = "OK"
+    char out[5];
+    sprintf(out, "%s%", "KEY", publicKey);
+
+    assertEqual(out, "KEYOK")
+}
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);
+  }
+  
+void loop() {
+    Test::run();
+}
